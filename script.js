e0ed76f0a0035f04590878ebfb25cc7406da22eb
eddysantiagoo <eddysantiagogh@gmail.com>
2022-06-09 13:05:40 -0500

    Scroll Reveal agregado

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ menubar.onclick = () =>{
     nav.classList.toggle('active')
   
 }
+
+const sr = ScrollReveal ({
+    distance : '45px',
+    duration : 2700,
+    reset : true,
+  })
+
+  sr.reveal('.home-content',{ delay:200, origin:'left' })
+  sr.reveal('.home-text-content',{ delay:350, origin:'right' })
+  sr.reveal('.inner-services',{ delay:800, origin:'bottom' })
+  sr.reveal('.inner-company',{ delay:800, origin:'top' })
+  sr.reveal('.company-text',{ delay:800, origin:'left' })
+  sr.reveal('.company-text-2',{ delay:800, origin:'right' })
+  sr.reveal('.footer',{ delay:50, origin:'bottom' })
