118fd292cd69a036962bab6c207a58124d6f3f04
eddysantiagoo <eddysantiagogh@gmail.com>
2022-06-14 15:52:39 -0500

    Agregar formulario contacto

--- script.js
+++ script.js
@@ -15,8 +15,8 @@ const sr = ScrollReveal ({
 
   sr.reveal('.home-content',{ delay:200, origin:'left' })
   sr.reveal('.home-text-content',{ delay:350, origin:'right' })
-  sr.reveal('.inner-services',{ delay:800, origin:'bottom' })
-  sr.reveal('.inner-company',{ delay:800, origin:'top' })
-  sr.reveal('.company-text',{ delay:800, origin:'left' })
-  sr.reveal('.company-text-2',{ delay:800, origin:'right' })
-  sr.reveal('.footer',{ delay:50, origin:'bottom' })
+  sr.reveal('.inner-services',{ delay:500, origin:'bottom' })
+  sr.reveal('.inner-company',{ delay:500, origin:'top' })
+  sr.reveal('.company-text',{ delay:500, origin:'left' })
+  sr.reveal('.company-text-2',{ delay:500, origin:'right' })
+  sr.reveal('.contact',{ delay:500, origin:'bottom' })
