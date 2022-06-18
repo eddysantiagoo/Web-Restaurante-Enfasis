e13c6e21225920ab98ae459f08f8f00ba4aabb50
eddysantiagoo <eddysantiagogh@gmail.com>
2022-06-18 10:05:18 -0500

    Agregar validaciones al formulario

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Menu responsive
+
 let menubar = document.querySelector('#menu-bar')
 let nav = document.querySelector('.navbar')
 
@@ -6,6 +8,97 @@ menubar.onclick = () =>{
     nav.classList.toggle('active')
   
 }
+
+// Validaciones formulario
+
+let nameError = document.getElementById('name-error');
+let phoneError = document.getElementById('phone-error');
+let emailError = document.getElementById('email-error');
+let messageError = document.getElementById('message-error');
+let submitError = document.getElementById('submit-error');
+
+
+function validateName(){
+    let name = document.getElementById('contact-name').value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if(!name.match (/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validatePhone(){
+    let phone = document.getElementById('contact-phone').value
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone is required';
+        return false;
+    }
+
+    if (phone.length !== 10) {
+       phoneError.innerHTML = 'Phone no should be 10 digits'
+        return false;
+    }
+
+    if (!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'Write only digits please'
+        return false;
+    }
+
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateEmail(){
+    let email = document.getElementById('contact-email').value
+    
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required'
+        return false;
+    }
+    if(!email.match (/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email is invalid'
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+    return true;
+
+}
+
+function validateMessage(){
+    let message = document.getElementById('contact-message').value
+    let required = 30;
+    let left = required - message.length;
+
+    if (left > 0) {
+        messageError.innerHTML = left + ' More characters required';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+    return true;
+
+}
+
+function validateForm(){
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+        submitError.innerHTML = 'Check the errors and resolve to submit please :)'
+        swal("Oops!", "Check the errors and resolve to submit please :)", "error");
+        return false;
+    } else {
+        swal("Perfect!", "Thank you, the form has been sent perfectly :)", "success");
+    }
+}
+
+
+// Efecto scroll reveal
 
 const sr = ScrollReveal ({
     distance : '45px',
